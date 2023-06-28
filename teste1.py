8bed9e39099356d559d8f81b02e28d728fa30c32
machado.patricia <patricia@computacao.ufcg.edu.br>
2023-06-28 16:50:36 -0300

    primeiro dia

--- teste1.py
+++ teste1.py
@@ -1,2 +1,10 @@
-idade = input("Digite sua idade: ")
-print(f"Você tem {idade} anos")
+idade = int(input("Digite sua idade: "))
+anoatual = int(input("Digite o ano: "))
+print(idade + 2050 - anoatual)
+if idade >= 60:
+    print("Você ê idosó")
+elif idade >= 18:
+    print("Você é adulto")
+else:
+    print("Você é jovem")
+
