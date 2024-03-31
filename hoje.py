f94aa8640d752726a7c031a6e16331616349c8e2
pdlmachado <patricia@computacao.ufcg.edu.br>
2024-03-31 18:42:25 -0300

    hoje

---
+++ hoje.py
@@ -0,0 +1,7 @@
+temperature = int(input("What's the temperature today? "))
+if temperature >= 30:
+    print ("it's a hot day!")
+elif temperature < 20:
+    print ("It's a cold day!")
+else:
+    print("It's a lovely day!")
